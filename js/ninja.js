6f411c2a578ee1c4dfb8317d5155d7c9f398d37d
Darrin <125dperez@frhsd.com>
2023-12-15 08:23:40 -0500

    video 6

--- js/ninja.js
+++ js/ninja.js
@@ -1,9 +1,8 @@
-var books = document.querySelectorAll('#book-list li .name')
+const banner = doucment.querySelector('#page-banner');
 
-Array.from(books).forEach(function(books){
-  books.textContent += '(book title)';
-});
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
-const booklist = document.querySelector('#book-list');
-//bookList.innerHTML = '<h2>Books and more books...</h2>';
-booklist.innerHTML += '<p>This is how you add HTML</p>';
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
