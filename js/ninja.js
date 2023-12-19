aec95092ca2f3091633f37cb5ab0e0ebab62d910
Darrin <125dperez@frhsd.com>
2023-12-19 10:43:28 -0500

    video 13

--- js/ninja.js
+++ js/ninja.js
@@ -13,7 +13,7 @@ list.addEventListener('click', (e) => {
 const addForm = forms['add-book'];
 addForm.addEventListener('submit', function(e){
   e.preventDefault();
-  
+
   // create elements
   const value = addForm.querySelector('input[type="text"]').value;
   const li = document.createElement('li');
@@ -24,9 +24,12 @@ addForm.addEventListener('submit', function(e){
   bookName.textContent = value;
   deleteBtn.textContent = 'delete';
 
+  // add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
   // append to DOM
   li.appendChild(bookName);
   li.appendChild(deleteBtn);
   list.appendChild(li);
-  //list.insertBefore(li, list.querySelector('li:first-child'));
 });
